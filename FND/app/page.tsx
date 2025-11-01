ce1bdfd5f222dc7fd019a05a30fece199f813787
ErickGreberSilva <pumpkinheadmetal@hotmail.com>
2025-11-01 17:38:47 +0000

    SUNPATHDONE

--- FND/app/page.tsx
+++ FND/app/page.tsx
@@ -9,12 +9,17 @@ import LoteValores from "@/components/Consulta/LoteValores";
 // import LoteExtrudeMap from "@/components/Consulta/LoteExtrudeMap";
 // import ProtomapsAPIMap from "@/components/Consulta/Prtomps";
 import SunPathCuritiba from "@/components/Consulta/sunpath";
-import SunProvider from "@/components/SunProvider";
 import SunPathStereographic from "@/components/SunDiargram";
+import Pagee from "@/components/Sunpaths/sunpath3";
 
 export default function Home() {
   return (
-    <SidebarProvider style={{ "--sidebar-width": "calc(var(--spacing) * 72)", "--header-height": "calc(var(--spacing) * 12)", } as React.CSSProperties}>
+    <SidebarProvider style=
+    {
+      { 
+      "--sidebar-width": "calc(var(--spacing) * 72)", 
+      "--header-height": "calc(var(--spacing) * 12)", 
+      } as React.CSSProperties}>
       <AppSidebar variant="inset" />
       <SidebarInset>
         <SiteHeader />
@@ -22,12 +27,11 @@ export default function Home() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 pt-0 md:gap-6 md:py-6 md:pt-0">
               <Potencial />
-              {/* <MAPA /> */}
               <div className="px-4 lg:px-6">
                 {/* <LoteExtrudeMap /> */}
-                <SunPathCuritiba />
-                <SunProvider />
+                <SunPathCuritiba /> {/* SVG by Elyson */}
                 <SunPathStereographic />
+                <Pagee />
                 {/* <ProtomapsAPIMap /> */}
                 <PotencialTable />
                 <QuadroZoneamento />
